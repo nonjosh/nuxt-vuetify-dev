144c94c320b4e1d704987c02e72cc2ab9299ceb7
nonjosh <nonjosh@gmail.com>
2021-09-03 13:41:36 +0000

    replace no-ssr with client-only in leaflet page

--- pages/leaflet.vue
+++ pages/leaflet.vue
@@ -9,12 +9,12 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <no-ssr>
+    <client-only>
       <l-map :zoom="zoom" :center="[55.9464418,8.1277591]">
         <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
         <l-marker :lat-lng="[55.9464418,8.1277591]" @click="dialog = true"></l-marker>
       </l-map>
-    </no-ssr>
+    </client-only>
   </div>
 </template>
 
