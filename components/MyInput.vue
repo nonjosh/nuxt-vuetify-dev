a06f610caef78fd72a586cad592b7779ea9274c1
Johnson Wong <cswong@hko.hksarg>
2021-09-27 04:33:14 +0000

    add missing MyInput component

---
+++ components/MyInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    My Input:
+    <input v-model="inputVal" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  computed: {
+    inputVal: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      },
+    },
+  },
+}
+</script>
+
+<style>
+</style>
