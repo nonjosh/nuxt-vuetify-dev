f100aea5862408511c0b106c43ea886381ec4b42
Johnson Wong <cswong@hko.hksarg>
2021-09-27 14:31:41 +0800

    add v-date-picker binding example

---
+++ components/MyDatePicker.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-date-picker v-model="inputVal" show-adjacent-months></v-date-picker>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'myDate',
+    event: 'change',
+  },
+  props: {
+    myDate: String,
+  },
+  computed: {
+    inputVal: {
+      get() {
+        return this.myDate
+      },
+      set(val) {
+        console.log(val)
+        this.$emit('change', val)
+      },
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- pages/binding.vue
+++ pages/binding.vue
@@ -38,6 +38,10 @@
         /></v-col>
       </v-row>
       <v-row>
+        <v-col cols="12" lg="3">myDate: {{ myDate }}</v-col>
+        <v-col cols="12" lg="4"> <my-date-picker v-model="myDate" /></v-col>
+      </v-row>
+      <v-row>
         <v-col cols="12" lg="3">foo: {{ foo }}</v-col>
         <v-col cols="12" lg="2"> <my-input v-model="foo"></my-input></v-col>
         <v-col cols="12" lg="2">
@@ -58,6 +62,7 @@ export default {
         warn: false,
       },
       foo: 'bar',
+      myDate: '2021-09-11',
     }
   },
 }
